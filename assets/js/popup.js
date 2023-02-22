158d43718096151527402734cd58445196aee9ba
Wutthiwat Sornso <wutthiwat.sor@truedigital.com>
2023-02-22 12:28:28 +0700

    fix: reset err

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -4,11 +4,10 @@ const reset = document.getElementById("notify-reset");
 
 reset.addEventListener("click", () => {
   chrome.storage.local.clear();
-  text.value = "";
 });
 
 notify.addEventListener("click", () => {
   chrome.runtime.sendMessage("", {
-    type: "notification"
+    type: "notification",
   });
 });
